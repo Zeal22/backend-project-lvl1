2ef92fe6e867f70eb177009da6634d2a13e5fe00
Makhmudov <fevral12@mail.ru>
2022-02-12 18:34:00 +0500

    fix problems

--- src/cli.js
+++ src/cli.js
@@ -1,4 +1,4 @@
-import readline from "readline-sync";
-console.log('Welcome to the Brain Games!');
+import readlineSync from "readline-sync";
+
 export const userName = readlineSync.question("May I have your name? ");
 console.log(`Hello, ${userName}!`);
