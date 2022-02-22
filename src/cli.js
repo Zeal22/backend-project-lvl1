688a3e25ecab609a4c57ad73dba65cc25ad9b022
Zeal22 <98512211+Zeal22@users.noreply.github.com>
2022-02-22 14:58:39 +0500

    Update cli.js

--- src/cli.js
+++ src/cli.js
@@ -1,6 +1,11 @@
 import readlineSync from 'readline-sync';
 
-console.log('Welcome to the Brain Games!');
-const userName = readlineSync.question('May I have your name? ');
-console.log(`Hello, ${userName}!`);
-export default userName;
+const greeting = () => {
+  console.log('Welcome to the Brain Games!');
+  
+  const name = readlineSync.question('May I have your name? ');
+  
+  console.log(`Hello, ${userName}!`);
+};
+
+export default () => greeting();
