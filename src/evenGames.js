517ff9c364934f0d4bc171263ca282f1cfc9de46
Zeal22 <98512211+Zeal22@users.noreply.github.com>
2022-02-23 13:35:05 +0500

    Update evenGames.js

--- src/evenGames.js
+++ src/evenGames.js
@@ -2,6 +2,19 @@ import readlineSync from 'readline-sync';
 import randomGenerator from '../src/randomizer.js';
 
 const number = randomGenerator(2, 11);
+const rounds = 3
+
+const generateCorrectAnswer = () => {
+  const number = generateRandomNum(2, 11);
+
+  const question = number.toString();
+
+  const correctAnswer = isEven(number) ? 'yes' : 'no';
+
+  return {
+    question,
+    correctAnswer,
+  };
 
 const greeting = () => {
   console.log('Welcome to the Brain Even!');
@@ -11,8 +24,24 @@ const greeting = () => {
   console.log(`Hello, ${name}!`);
   console.log(`Answer "yes" if the number is even, otherwise answer "no".`);
   
-  console.log(`Question: ${number}`);
-  const answer = readlineSync.question('Your answer: ');
+  for (let i = 0; i < rounds; i += 1) {
+    const { question, correctAnswer } = generateCorrectAnswer();
+
+    console.log(`Question: ${question}`);
+
+    const userAnswer = readlineSync.question('Your answer: ');
+
+    if (correctAnswer !== userAnswer) {
+      console.log(
+        `${userAnswer} these is wrong answer. Correct answer is ${correctAnswer}. Lets try again ${name}!`,
+      );
+      return;
+    }
+    console.log('Correct!');
+  }
+
+  console.log(`Congratulations, ${name} you won!`);
+};
     
 };
 
