69722b042ab7b72762382928bc72bfab22084b00
Zeal22 <98512211+Zeal22@users.noreply.github.com>
2022-02-23 11:53:13 +0500

    Update evenGames.js

--- src/evenGames.js
+++ src/evenGames.js
@@ -1,4 +1,5 @@
 import readlineSync from 'readline-sync';
+import randonGenerator from '../randomizer.js';
 
 const greeting = () => {
   console.log('Welcome to the Brain Even!');
@@ -6,6 +7,10 @@ const greeting = () => {
   const name = readlineSync.question('May I have your name? ');
 
   console.log(`Hello, ${name}!`);
+  console.log(`Answer "yes" if the number is even, otherwise answer "no".`);
+  
+  
+  const 
 };
 
 export default () => greeting();
