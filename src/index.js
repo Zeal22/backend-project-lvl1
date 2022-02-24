49cb8cb6de3e2d1ec80527cda80ca7c52694b7f1
Zeal22 <fevral12@mail.ru>
2022-02-24 14:01:47 +0500

    Revert "Create index.js"
    This reverts commit 9cfff5492bb7bb5c267ae6ee5858ce3021277f6e.

---
+++ src/index.js
@@ -0,0 +1,24 @@
+import readlineSync from 'readline-sync';
+
+const rounds = 3;
+const newGames = (generateGame, questionToStart) => {
+console.log('Welcome to the Brain Even!');
+const name = readlineSync.question('May I have your name? ');
+console.log(`Hello, ${name}!`);
+console.log(questionToStart);
+
+  for (let i = 0; i < rounds; i += 1) {
+    const { question, correctAnswer } = generateGame();
+
+    console.log(`Question: ${question}`);
+    const userAnswer = readlineSync.question('Your answer: ');
+    if (correctAnswer !== userAnswer) {
+      console.log(`${userAnswer} these is wrong answer ;(. Correct answer is ${correctAnswer}. Lets try again ${name}!`);
+      return;
+    }
+    console.log('Correct!');
+  }
+  console.log(`Congratulations, ${name} you won!`);
+};
+
+export default newGames;
