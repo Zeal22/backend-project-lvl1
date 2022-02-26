d416ccd94e59cb38ebec391a530973ed218624b3
Zeal22 <fevral12@mail.ru>
2022-02-26 21:14:52 +0500

    progression

--- src/games/brainProgression.js
+++ src/games/brainProgression.js
@@ -5,27 +5,35 @@ const questionToStart = 'What number is missing in the progression?';
 
 const massiveLength = 10;
 
-const isProgression = () => {
-  const numArray = [];
-  const interval = randomGenerator(2, 5);
-  let temp = 0;
-  for (let i = 0; i <= massiveLength; i += 1) {
-    temp += interval;
-    numArray.push(temp);
+const isProgression = (
+  numberToStartProgression,
+  stepInProgression,
+) => {
+  const progression = [];
+  for (
+    let i = numberToStartProgression;
+    progression.length < massiveLength;
+    i += stepInProgression
+  ) {
+    progression.push(i);
   }
-  return numArray;
+  return progression;
 };
 
 const generateQuestion = () => {
-  const numArray = isProgression(randomGenerator(5, 10));
+  const progression = isProgression(
+    randomGenerator(2, 11),
+    randomGenerator(2, 11),
+    massiveLength,
+  );
 
   const missingNumber = randomGenerator(0, massiveLength);
 
-  const correctAnswer = numArray[missingNumber].toString();
+  const correctAnswer = progression[missingNumber].toString();
 
-  numArray[missingNumber] = '..';
+  progression[missingNumber] = '..';
 
-  const questionStr = numArray.join(', ');
+  const questionStr = progression.join(', ');
 
   return {
     question: questionStr,
