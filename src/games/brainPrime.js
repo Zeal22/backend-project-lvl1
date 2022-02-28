f50c0c718827020f95104d54b6d00ed6dd2baa7a
Zeal22 <fevral12@mail.ru>
2022-02-28 11:52:06 +0500

    prime fix

--- src/games/brainPrime.js
+++ src/games/brainPrime.js
@@ -3,10 +3,18 @@ import randomGenerator from '../randomizer.js';
 
 const questionToStart = '"yes" if given number is prime. Otherwise answer "no".';
 
-const isPrime = (num) => num % 2 || num % 3 === 0;
+const isPrime = (num) => {
+  if (num % 2 === 0) {
+    return false;
+  }
+  if (num % 3 === 0) {
+    return false;
+  }
+  return true;
+};
 
 const generateQuestion = () => {
-  const number = randomGenerator(3, 11);
+  const number = randomGenerator(3, 20);
 
   const question = number.toString();
 
